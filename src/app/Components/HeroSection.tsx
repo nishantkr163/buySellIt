d4aae297e5862dadfa12268693cec830dc32ba9b
Nishant Singh <nishant.kumar@bandhoo.com>
2025-04-15 21:55:20 +0530

    Hero section and Header sub header completed

---
+++ src/app/Components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CarouselCommon from './CarouselCommon'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <CarouselCommon />
+    </div>
+  )
+}
+
+export default HeroSection
